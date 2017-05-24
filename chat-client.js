39b55c0ebcac7edae87e5390a1f350d48c70bcb9
david <david@davids-MacBook-Pro.local>
2017-05-23 21:02:58 -0700

    base test chat

---
+++ chat-client.js
@@ -0,0 +1,14 @@
+/**
+ * test chat client for we server
+ */
+var ws       = require( "ws" );
+var NetUtils = require( "../libs/node-lib/client-server/NetUtils" );
+var client   = new ws( "ws://172.20.10.10:" + NetUtils.CommonPorts.DEVICE_WS );
+
+client.on('open', function open() {
+  ws.send('something');
+});
+
+client.on('message', function incoming( data ) {
+  console.log( data );
+});
--- server.js
+++ server.js
@@ -1,18 +1,31 @@
 /**
  * the bid red button server
  */
-var ws       = require("nodejs-websocket")
+var ws       = require( "ws" );
 var NetUtils = require( "../libs/node-lib/client-server/NetUtils" );
+var server   = new ws.Server( { port : NetUtils.CommonPorts.DEVICE_WS } );
 
-var server = ws.createServer(function( conn ) {
-	console.log( "New connection" )
+server.on( 'connection', function connection( wsClient ) {
+	wsClient.on( 'message', function incoming( message ) {
+		console.log('received: %s', message);
+  });
+
+	ws.send( 'something' );
+});
+
+
+// var ws       = require("nodejs-websocket")
+// var NetUtils = require( "../libs/node-lib/client-server/NetUtils" );
+
+// var server = ws.createServer(function( conn ) {
+// 	console.log( "New connection" )
 	
-	conn.on( "text", function(str) {
-		console.log("Received "+str)
-		conn.sendText(str.toUpperCase()+"!!!")
-	})
+// 	conn.on( "text", function(str) {
+// 		console.log("Received "+str)
+// 		conn.sendText(str.toUpperCase()+"!!!")
+// 	})
 
-	conn.on("close", function (code, reason) {
-		console.log("Connection closed")
-	})
-}).listen(NetUtils.CommonPorts.DEVICE_WS);
+// 	conn.on("close", function (code, reason) {
+// 		console.log("Connection closed")
+// 	})
+// }).listen(NetUtils.CommonPorts.DEVICE_WS);
