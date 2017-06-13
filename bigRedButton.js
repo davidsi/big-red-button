be3f4fa356d03490434e977ae6a1bc8460fb04f9
davidsi <davidsi@mac.com>
2017-06-13 07:51:59 -0700

    automated commit

--- bigRedButton.js
+++ bigRedButton.js
@@ -3,15 +3,13 @@
  */
 var WebSocket             = require( "ws" );
 var NetUtils              = require( "../libs/node-lib/client-server/NetUtils" );
-var socketServer          = new WebSocket.Server( { port : NetUtils.CommonPorts.DEVICE_WS } );
 var pinManager            = require( "../libs/node-lib/device/chip/gpio/pinManager" );
 var httpServer            = require( "../libs/node-lib/client-server/server" );
 var Router                = require( "../libs/node-lib/client-server/router" );
-var ButtonRequestHandlers = require( "./buttonRequestHandlers" );
 
-var button       = pinManager.getPin( 0, false, 'in', 'both', { debounceTimeout: 500 } );
-
-need to add the B-R-B as a regular device
+var socketServer          = new WebSocket.Server( { port : NetUtils.CommonPorts.DEVICE_WS } );
+var button                = pinManager.getPin( 0, false, 'in', 'both', { debounceTimeout: 500 } );
+var buttonID			  = 102;
 
 socketServer.on( 'connection', function connection( wsClient ) {
 
@@ -26,6 +24,64 @@ socketServer.on( 'connection', function connection( wsClient ) {
 });
 
 /**
+ * get the node description
+ */
+function query() {
+	
+    this.responder = function( response, request, params ) {
+
+    	var resp = {
+
+		"SSID-config"             : false,								
+		"bluetooth-service-guid"  : "",									
+		"nodeType"                : ["big-red-button"],
+		"big-red-button-reciever" : false,
+		"name"                    : "iTunes big red button",
+		"hardware"                : "chip",
+		"battery"                 : false,								
+		"id"		              : buttonID,								
+		"configable"              : "http",
+		"big-red-button" : {											
+			"queryAvailable" : ["buttonID"],
+			"buttonID"       : buttonID
+		}
+
+        response.writeHead( 200, {"Content-Type": "application/json", });
+        response.write( JSON.stringify(resp) );
+        response.end(); 
+    }
+}
+
+/**
+ * get/set the BRB specifics
+ */
+function data {
+
+   this.responder = function( response, request, params ) {
+
+    	var resp = {
+
+		"SSID-config"             : false,								
+		"bluetooth-service-guid"  : "",									
+		"nodeType"                : ["big-red-button"],
+		"big-red-button-reciever" : false,
+		"name"                    : "iTunes big red button",
+		"hardware"                : "chip",
+		"battery"                 : false,								
+		"id"		              : buttonID,								
+		"configable"              : "http",
+		"big-red-button" : {											
+			"queryAvailable" : ["buttonID"],
+			"buttonID"       : buttonID
+		}
+
+        response.writeHead( 200, {"Content-Type": "application/json", });
+        response.write( JSON.stringify(resp) );
+        response.end(); 
+    }
+}
+
+/**
  * watch the button, if we get a hit, we need to broadcast a message to all clients
  */
 button.watch( function( error, value ) {
@@ -34,7 +90,7 @@ button.watch( function( error, value ) {
 
 	socketServer.clients.forEach(function each( client ) {
 		if( client.readyState === WebSocket.OPEN ) {
-			client.send('big red button!');
+			client.send( JSON.stringify( {"big-red-button" : buttonID} )) ;
 		}
 		else {
 			console.log( "client not open any more" );
@@ -42,11 +98,10 @@ button.watch( function( error, value ) {
 	});
 });
 
-console.log( "starting weather station" );
+console.log( "starting big red button" );
 
-var query = new ButtonRequestHandlers.query;
-router.addRoute( "/query", query);
-router.addRoute( "/q",     query );
+router.addRoute( "/query", new query() );
+router.addRoute( "/data",  new data() );
 
 httpServer.start( new Router() );
 
