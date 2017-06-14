1f6f375afd5bde08db0612b85967e73c9b77b378
davidsi <davidsi@mac.com>
2017-06-13 21:56:22 -0700

    bugs

--- bigRedButton.js
+++ bigRedButton.js
@@ -44,7 +44,7 @@ function query() {
 		"big-red-button" : {											
 			"queryAvailable" : ["buttonID"],
 			"buttonID"       : buttonID
-		}
+		};
 
         response.writeHead( 200, {"Content-Type": "application/json", });
         response.write( JSON.stringify(resp) );
