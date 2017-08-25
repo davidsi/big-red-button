0199241b2ee240eba4dc46ef5904c35085c567b2
davidsi <davidsi@mac.com>
2017-08-24 23:30:23 -0700

    automated commit

---
+++ chat-client-example.js
@@ -0,0 +1,29 @@
+/**
+ * test chat client for we server
+ */
+var ws       = require( "ws" );
+var NetUtils = require( "../libs/node-lib/client-server/NetUtils" );
+var client   = new ws( "ws://172.20.10.10:"+NetUtils.CommonPorts.DEVICE_WS );
+var shelljs  = require('shelljs');
+
+client.on('open', function open() {
+  client.send('something');
+});
+
+client.on('message', function incoming( data ) {
+
+	console.log( "recieved " + data );
+
+	if( data == "connected" ) {
+		console.log( "connection alive" );
+	}
+	else if( data == "big red button!" ) {
+
+		var script1  = 'display dialog "Hello from david" with title "Hello"';
+		var script   = 'tell application "iTunes" to play playlist "Music"';
+		var execStr  = "osascript -e '" + script + "'";
+
+		console.log( execStr );
+		shelljs.exec( execStr );
+	}
+});
